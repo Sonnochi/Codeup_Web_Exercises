1cba6f689e2b3464081bb5d6f90be44efd82b475
Sonnochi <joseefarmer@yahoo.com>
2014-10-21 15:53:09 -0500

    java array sheet

---
+++ public/js/iterating.js
@@ -0,0 +1,26 @@
+// todo:
+// Create an array of 4 names using literal array notation, in a variable called 'names'.
+
+var names = ['Bob', 'Xzavior', 'Zeak', 'Malcome'];
+
+// todo:
+// Create a log statement that will log the number of elements in the names array.
+console.log(names);
+
+// There are 4 names in the array
+console.log('There are ' + names.length + ' in the array.'); 
+
+// todo:
+// Create log statements that will print each of the names array elements individually.
+
+// The first name is: Bob
+console.log('The first name is: ' + names[0]);
+
+// The second name is: Xzavior
+console.log('The second name is: ' + names[1]);
+
+// The third name is: Zeak
+console.log('The third name is: ' + names[2]);
+
+// The fourth name is: Malcome
+console.log('The fourth name is: ' + names[3]);
