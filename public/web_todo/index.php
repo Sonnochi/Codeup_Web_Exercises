417707365e200bb5b9f44d8a5810f6a94cf5ea6c
Sonnochi <joseefarmer@yahoo.com>
2014-11-03 15:04:18 -0600

    ToDo list added a require function

--- public/web_todo/index.php
+++ public/web_todo/index.php
@@ -1,46 +1,8 @@
 <?php
  
- class ToDoList{
 
- 	// public $name = '';
- 	public $filename = 'list.txt';
- 	public $items = [];
-	// Define a function which will open your default filename, and return an array of items.
+ require'../../inc/indexClass.php';
 
-	/* This function accepts an array, saves it to file, and returns an array of list items. */ 
-	public function openFile(){
-		$contentArray = [];
-
-		//If the filesize is greater that zero, use it.
-		if (file_exists($this->filename) && filesize($this->filename) > 0) {
-			$handle = fopen($this->filename, 'r');
-			$contents = trim(fread($handle, filesize($this->filename)));
-			$contentArray = explode("\n", $contents);
-			$filesize = filesize($this->filename);
-			fclose($handle);
-		}
-		// var_dump($contentArray);
-		return $contentArray;
-	}
- 
-
-	// Define a function which will save your list to file.
-
-	/* This function accepts an array, saves it to file, and returns nothing. */
-	public function saveFile($array){
-
-		foreach ($array as $key => $value) {
-			$array[$key] = htmlspecialchars(strip_tags($value));
-		}
-
-		$handle = fopen($this->filename, 'w');
-		// Implode the entire array into one string, with newlines in between each item
-		$string = implode("\n", $array);
-		//Write that whole sting to file.
-		fwrite($handle, $string);
-		fclose($handle);
-	}
-}
 // Initialize your array by calling your function to open file.
 
 // Create a new instance of Conversation
@@ -53,24 +15,24 @@ $items = $itemsList->openFile();
 
 // Verify there were uploaded files and no errors
 if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK && $_FILES['file1']['error'] == 'text/plain'){
-    // Set the destination directory for uploads
-    $uploadDir = '/vagrant/sites/codeup.dev/public/uploads/';
+	    // Set the destination directory for uploads
+	    $uploadDir = '/vagrant/sites/codeup.dev/public/uploads/';
 
-    // Grab the filename from the uploaded file by using basename
-    $filename = basename($_FILES['file1']['name']);
+	    // Grab the filename from the uploaded file by using basename
+	    $filename = basename($_FILES['file1']['name']);
 
-    // Create the saved filename using the file's original name and our upload directory
-    $savedFilename = $uploadDir . $filename;
+	    // Create the saved filename using the file's original name and our upload directory
+	    $savedFilename = $uploadDir . $filename;
 
-    // Move the file from the temp location to our uploads directory
-    move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
+	    // Move the file from the temp location to our uploads directory
+	    move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
 
-    //opening uploading file and merging onto existing array
-    $new_array = openFile('../uploads/' . $filename);
-    $items = array_merge($items, $new_array);
-    
-    // save function
-    saveFile($items);
+	    //opening uploading file and merging onto existing array
+	    $new_array = openFile('../uploads/' . $filename);
+	    $items = array_merge($items, $new_array);
+	    
+	    // save function
+	    saveFile($items);
 
 
 }
